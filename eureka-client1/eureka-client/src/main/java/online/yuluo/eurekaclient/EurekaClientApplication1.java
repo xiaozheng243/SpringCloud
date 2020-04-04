1340b164ed19992c2c045b398e17c589cc3ee9c5
xiaozheng243 <luzz_1@163.com>
2020-04-04 18:48:49 +0800

    eureka client

--- eureka-client1/eureka-client/src/main/java/online/yuluo/eurekaclient/EurekaClientApplication1.java
+++ eureka-client1/eureka-client/src/main/java/online/yuluo/eurekaclient/EurekaClientApplication1.java
@@ -2,16 +2,14 @@ package online.yuluo.eurekaclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
-@LoadBalancerClient
 @SpringBootApplication
-public class EurekaClientApplication {
+public class EurekaClientApplication1 {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EurekaClientApplication.class, args);
+		SpringApplication.run(EurekaClientApplication1.class, args);
 	}
 
 }
