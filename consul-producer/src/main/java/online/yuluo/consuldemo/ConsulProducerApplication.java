a89ae8486daaf6b7fcc496b5e1468b42d6e88928
xiaozheng243 <luzz_1@163.com>
2020-04-06 15:15:09 +0800

    服务生产者

---
+++ consul-producer/src/main/java/online/yuluo/consuldemo/ConsulProducerApplication.java
@@ -0,0 +1,13 @@
+package online.yuluo.consuldemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsulProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsulProducerApplication.class, args);
+	}
+
+}
